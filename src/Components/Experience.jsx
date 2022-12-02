33e0aea8a84ce2a8cc87866e060e31ccd33a99a1
Kalab Amssalu <Kalishdesandy@gmail.com>
2022-12-02 15:08:37 +0300

    php

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -5,7 +5,7 @@ import css from "../assets/css.png";
 import javascript from "../assets/javascript.png";
 import reactImage from "../assets/react.png";
 import nextjs from "../assets/nextjs.png";
-import graphql from "../assets/graphql.png";
+import php from "../assets/PHP.png";
 import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
 
@@ -49,8 +49,8 @@ const Experience = () => {
     },
     {
       id: 7,
-      src: graphql,
-      title: "GraphQL",
+      src: php,
+      title: "Php",
       style: "shadow-pink-400",
     },
     {
